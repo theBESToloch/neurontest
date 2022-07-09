e1583cd7ac794e9f359ae7ffa4d001882ebb54e1
pro100pzdec1998 <107275012+pro100pzdec1998@users.noreply.github.com>
2022-07-09 21:34:52 +0300

    my first commit

--- UI/src/main/java/com/test/persistence/services/NNPreviewService.java
+++ UI/src/main/java/com/test/persistence/services/NNPreviewService.java
@@ -2,24 +2,11 @@ package com.test.persistence.services;
 
 import com.test.persistence.entities.NNPreview;
 import com.test.persistence.repositories.NNPreviewRepository;
-import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.time.LocalDateTime;
 
-@Service
-public class NNPreviewService {
-
-    private final NNPreviewRepository nnPreviewRepository;
-
-    public NNPreviewService(NNPreviewRepository nnPreviewRepository) {
-        this.nnPreviewRepository = nnPreviewRepository;
-    }
-
-    public NNPreview save(ByteArrayOutputStream imagePreview) {
-        NNPreview nnPreview = new NNPreview()
-                .setDate(LocalDateTime.now())
-                .setPreviewImage(imagePreview.toByteArray());
-        return nnPreviewRepository.save(nnPreview);
-    }
+public interface NNPreviewService {
+    NNPreview save(ByteArrayOutputStream imagePreview);
+    NNPreview nnPreview = new NNPreview();
 }
