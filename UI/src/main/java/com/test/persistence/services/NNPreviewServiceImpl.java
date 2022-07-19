fb1d9c877aac1dc14bdeb30558f796a9b0a79178
pro100pzdec1998 <107275012+pro100pzdec1998@users.noreply.github.com>
2022-07-19 21:58:55 +0300

    Пправил конфликт,заебался)

--- UI/src/main/java/com/test/persistence/services/NNPreviewServiceImpl.java
+++ UI/src/main/java/com/test/persistence/services/NNPreviewServiceImpl.java
@@ -2,6 +2,8 @@ package com.test.persistence.services;
 
 import com.test.persistence.entities.NNPreview;
 import com.test.persistence.repositories.NNPreviewRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -21,5 +23,16 @@ public class NNPreviewServiceImpl implements NNPreviewService {
                 .setDate(LocalDateTime.now())
                 .setPreviewImage(imagePreview.toByteArray());
         return nnPreviewRepository.save(nnPreview);
+    }
+
+    @Override
+    public Page<NNPreview> load(Pageable pageable) {
+        return  nnPreviewRepository.findAll(pageable);
+    }
+
+    @Override
+    public void delete(Long id) {
+        nnPreviewRepository.deleteById(id);
+
     }
 }
